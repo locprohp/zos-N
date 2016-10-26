3f93d2ea93f3f2e67b4df82e57b4a2b4e921b245
xuanloc <locprohp1995@gmail.com>
2016-10-26 14:57:19 +0700

    Update and rename xenonhd.mk to zos_cancro.mk

--- zos_cancro.mk
+++ zos_cancro.mk
@@ -19,16 +19,16 @@ TARGET_SCREEN_WIDTH := 1080
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
+# Inherit some common ZOS stuff.
+$(call inherit-product, vendor/zos/common.mk)
 
 # Inherit from cancro device
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/xenonhd/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/zos/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := xenonhd_cancro
+PRODUCT_NAME := zos_cancro
 PRODUCT_DEVICE := cancro
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
